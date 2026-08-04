8311868b181607d2a6027e771ed6bec9f87a7e95
hidesroom <ko.yunet@gmail.com>
2026-08-04 17:46:08 +0900

    課題：kadai_020 UP

---
+++ kadai_020/event.js
@@ -0,0 +1,7 @@
+const countBtn = document.getElementById('btn');
+
+countBtn.addEventListener('click', () => {
+
+  const text = document.getElementById('text');
+  text.textContent = 'ボタンをクリックしました';
+});
